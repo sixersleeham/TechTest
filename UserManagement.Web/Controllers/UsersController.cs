54718df1cf4f2254c662a8b795a412b8012cc45d
Liam <alexisthebaddog@gmail.com>
2025-08-15 20:25:13 +0100

    2. User Model Properties
    Completed Task 2 - Added the Date of Birth property to the User class and gave each user a random DOB in the DataContext. Updated the controller, model and View to present the date of birth in a new column.

--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -31,7 +31,8 @@ public class UsersController : Controller
             Forename = p.Forename,
             Surname = p.Surname,
             Email = p.Email,
-            IsActive = p.IsActive
+            IsActive = p.IsActive,
+            DateOfBirth = p.DateOfBirth
         });
 
         var model = new UserListViewModel
